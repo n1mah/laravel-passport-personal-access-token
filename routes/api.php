625e4faf0b403266d732634b430aafa93208a68c
Nima Heidari <nima.heidari.n@gmail.com>
2025-10-31 13:37:53 +0330

    feat: Add route login & user route with middleware api passport

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,18 @@
 <?php
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
+
+
+Route::get('/login', function (Request $request){
+    if (!Auth::attempt($request->only('email', 'password')))
+        return response()->json(['message' => 'Invalid credentials'], 401);
+    $user = Auth::user();
+    return response()->json([
+        'token' => $user->createToken('api')->accessToken
+    ]);
+});
 
 Route::get('/user', function (Request $request) {
     return $request->user();
